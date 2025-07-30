ffaada09d498a0f07bf7dd246a4b0d24008a9b6a
nadhilkn <nadhunadhil33429@gmail.com>
2025-07-30 09:24:16 +0530

    30-07-25

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,7 @@
+import { Header } from "./Header.jsx"
+export function App() {
+  return(
+   <Header/>
+  );
+}
+
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,6 @@
+ function Header(){
+    return(
+        <h1>hello !!</h1>
+    );
+}
+export default Header;
