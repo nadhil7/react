8745d0ffc212749e002152565ac68023f6490f96
nadhilkn <nadhunadhil33429@gmail.com>
2025-07-30 17:29:44 +0530

    last

--- flipkart/src/components/Header.jsx
+++ flipkart/src/components/Header.jsx
@@ -1,9 +1,27 @@
-export default function Header(){
-    return(
-        <header>
-        <div><img src="" alt="" /></div>
-        <div></div>
-
+import flipkarthead from '../assets/flipkart.png'
+import dpicon from '../assets/dpicon.svg'
+import arrowicon from '../assets/arrowimage.svg'
+import carticon from '../assets/carticon.svg'
+import 
+export default function Header() {
+    return (
+        <header className='flex'>
+            <div><img src={flipkarthead} alt="refresh icon flipkart" /></div>
+            <div> <input type="text" /></div>
+            <div>
+                {/* <ul>
+               <li><a href=""></a></li>
+               <li><a href=""></a></li>
+               <li><a href=""></a></li>
+               <li><a href=""></a>
+               
+               </li>
+            </ul> */}
+                <div className='flex'><img src={dpicon} alt="" /><h3>Login</h3><img src={arrowicon} alt="" /></div>
+                <div><img src={carticon} alt="" /></div>
+                <div><img src={} alt="" /></div>
+                <div><img src="" alt="" /></div>
+            </div>
         </header>
     );
 };
