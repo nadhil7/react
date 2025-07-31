50a3dfde2d38d90964ec55ec063d2375592f2d69
nadhilkn <nadhunadhil33429@gmail.com>
2025-07-31 12:46:18 +0530

    favicon

--- flipkart/src/components/Header.jsx
+++ flipkart/src/components/Header.jsx
@@ -2,12 +2,14 @@ import flipkarthead from '../assets/flipkart.png'
 import dpicon from '../assets/dpicon.svg'
 import arrowicon from '../assets/arrowimage.svg'
 import carticon from '../assets/carticon.svg'
+import headericon from '../assets/headericons.svg'
+import menudots from '../assets/menuicon.svg'
 export default function Header() {
     return (
-        <header className='flex'>
+        <header className='flex justify-between'>
             <div><img src={flipkarthead} alt="refresh icon flipkart" /></div>
-            <div> <input type="text" /></div>
-            <div>
+            <div><input type="text" className='border rounded-2xl ' /></div>
+            <div className='flex justify-evenly w-full'>
                 {/* <ul>
                <li><a href=""></a></li>
                <li><a href=""></a></li>
@@ -16,10 +18,10 @@ export default function Header() {
                
                </li>
             </ul> */}
-                <div className='flex'><img src={dpicon} alt="" /><h3>Login</h3><img src={arrowicon} alt="" /></div>
+                <div className=''><img src={dpicon} alt="" /><h3>Login</h3><img src={arrowicon} alt="" /></div>
                 <div><img src={carticon} alt="" /></div>
-                <div><img src={} alt="" /></div>
-                <div><img src="" alt="" /></div>
+                <div><img src={headericon} alt="" /></div>
+                <div><img src={menudots} alt="" /></div>
             </div>
         </header>
     );
