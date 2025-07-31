439a6422ebda2a95ef4e4175260cf354fd002c73
nadhilkn <nadhunadhil33429@gmail.com>
2025-07-31 15:38:52 +0530

    3rd

--- flipkart/src/App.jsx
+++ flipkart/src/App.jsx
@@ -1,9 +1,26 @@
+import category1 from './assets/imgi_18_a22a213ca6221b65.png'
+import category2 from './assets/imgi_19_5f2ee7f883cdb774.png'
+import category3 from './assets/imgi_20_ff559cb9d803d424.png'
+import category4 from './assets/imgi_21_af646c36d74c4be9.png'
+import category5 from './assets/imgi_22_1788f177649e6991.png'
+import category6 from './assets/imgi_24_3c647c2e0d937dc5.png'
+import category7 from './assets/imgi_25_b3020c99672953b9.png'
+import category8 from './assets/imgi_26_e730a834ad950bae.png'
 import Header from "./components/Header";
+import Card from './components/card'
+
+const array = [{ image: category1, name: "Minutes" }, { image: category2, name: "Mobile & Tablets" },
+{ image: category3, name: "Fashion" }, { image: category4, name: "Electronics" },
+{ image: category5, name: "Home & Furniture" }, { image: category6, name: "TV & Appliances" },
+{ image: category7, name: "Flight Bookings" }, { image: category8, name: "Beauty & Foods" }]
+
+
 function App() {
   return (
     <>
-    <Header></Header>
+      <Header></Header>
+      <section class></section>
     </>
-  );   
+  );
 }
 export default App
---
+++ flipkart/src/components/card.jsx
@@ -0,0 +1,10 @@
+ 
+function Card(){
+    return(
+        <div  className="Card">
+            <img src="" alt="profilepic" />
+            <h2></h2>
+        </div>
+    );
+}
+export default Card;
