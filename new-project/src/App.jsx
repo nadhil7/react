ce0a985680aea37fe70dc08a5edbc6f70a49a7ce
nadhilkn <nadhunadhil33429@gmail.com>
2025-08-02 15:48:42 +0530

    useeffect

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Hooks from './components/hooks'
 import Timeout from './components/timeout'
+import Useeffect from './components/Useeffect'
 
 function App() {
   return (
-  //  <>
-  //  <Hooks/>
-  //  </>
-  <>
-  <Timeout/>
-  </>
+    <>
+      {/* <Hooks/> */}
+      {/* <Timeout/> */}
+      <Useeffect />
+    </>
   )
 }
 
---
+++ new-project/src/components/Useeffect.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+export default function Useeffect() {
+
+
+    const [count, counter] = useState(0)
+    useEffect(() => {
+
+       const co = setInterval(() => {
+            counter(count + 1)
+        }, 1000)
+    })
+
+
+    function reset() {
+        counter(count - count)
+
+    }
+
+
+    return (
+        <>
+            <div>
+                <p>{count}</p>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </>
+    )
+}
